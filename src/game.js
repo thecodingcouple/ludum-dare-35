da2d8cfc27e043c7ed313fb24508dc93479f820e
townsean <ashley.grenon@gmail.com>
2016-04-16 12:40:40 -0500

    A few updates to the appearance of the website

--- src/game.js
+++ src/game.js
@@ -1,9 +1,9 @@
-import {Phaser} from 'phaser';
-import {Start} from './states/start'
+import { Phaser } from 'phaser';
+import { Start } from './states/start'
 
 export class Game extends Phaser.Game{
     constructor() {
-        super(500, 500, Phaser.AUTO, 'game-canvas', null);
+        super(800, 600, Phaser.AUTO, 'game-canvas', null);
         this.state.add('Start', Start);
         this.state.start('Start');
     }
--- src/states/start.js
+++ src/states/start.js
@@ -1,4 +1,4 @@
-import {Phaser} from 'phaser';
+import { Phaser } from 'phaser';
 
 export class Start extends Phaser.State{
     create() {
