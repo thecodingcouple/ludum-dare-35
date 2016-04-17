0ce08337a6d4435ce1e97c42b00af95ce0406b9b
townsean <ashley.grenon@gmail.com>
2016-04-17 16:12:36 -0500

    The monsters are chasing the wizard now

--- src/states/play.js
+++ src/states/play.js
@@ -10,14 +10,24 @@ export class Play extends Phaser.State {
     
     create() {
         
-        this.game.add.tileSprite(0, 0, 800, 600, 'background');
-        this.wizard = new Wizard(this.game);
-        this.monster = new Monster(this.game);
-               
-        this.game.add.existing(this.wizard);
-        this.game.add.existing(this.monster);
-        
+        this.game.add.tileSprite(0, 0, 800, 600, 'background');        
         this.game.physics.startSystem(Phaser.Physics.ARCADE); 
+        
+        this.wizard = new Wizard(this.game);
+        this.game.add.existing(this.wizard);
+        
+        this.monsters = this.game.add.group();
+        this.monsters.enableBody = true;
+        
+        for(let x = 0; x < 5; x++) {
+            
+            let monster = new Monster(this.game);            
+            this.monsters.add(monster);
+        }
+        
+        this.monsters.setAll('body.velocity.x', 0);
+        this.monsters.setAll('body.velocity.y', 0);
+        
         this.cursors = this.game.input.keyboard.createCursorKeys();       
         this.game.stage.backgroundColor = "#FFFFFF";
         
@@ -31,11 +41,13 @@ export class Play extends Phaser.State {
     
     update() {
         this.wizard.skate(this.cursors);
-        this.monster.chill();
+        this.monsters.callAll('munch');
         
         if (this.game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)) {                  
             this.wizard.castSpell();
             this.spell.play();
         }
+        
+        this.monsters.forEach(this.game.physics.arcade.moveToObject, this.game.physics.arcade, false, this.wizard, 1000, 1000);
     }
 }
