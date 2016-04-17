079b16ed8b01e7cf52cfb48a8c05eacd8481dec9
townsean <ashley.grenon@gmail.com>
2016-04-16 21:14:22 -0500

    Adding sound effect when the wizard casts a spell

--- src/states/load.js
+++ src/states/load.js
@@ -7,19 +7,17 @@ export class Load extends Phaser.State {
        this.game.add.text(80, 150, 'loading...', textStyle);
        this.game.load.image('wizard', '../assets/wizard.png');
        this.game.load.audio('background', '../assets/happy.mp3');
-       this.game.load.audio('spell', '../assets/spell1_0.wav');
     }
     
     create() {
         this.game.state.start('menu');
         
         this.background = this.game.add.audio('background');
-        this.spell = this.game.add.audio('spell');
         
-        this.game.sound.setDecodedCallback([ this.background, this.spell ], this.start, this)
+        this.game.sound.setDecodedCallback(this.background, this.start, this)
     }
     
     start() {
-        this.background.loopFull(0.6);
+        this.background.loopFull(0.4);
     }
 }
--- src/states/play.js
+++ src/states/play.js
@@ -2,6 +2,11 @@ import { Phaser } from 'phaser';
 import { Wizard } from '../sprites/wizard';
 
 export class Play extends Phaser.State {
+    preload() {
+        
+       this.game.load.audio('spell', '../assets/spell1_0.wav');
+    }
+    
     create() {
         this.wizard = new Wizard(this.game);
                
@@ -12,6 +17,11 @@ export class Play extends Phaser.State {
         this.game.stage.backgroundColor = "#FFFFFF";
         
         this.game.input.keyboard.addKeyCapture([ Phaser.KeyCode.SPACEBAR ]);
+        
+        
+        this.spell = this.game.add.audio('spell');
+        this.spell.volume = 0.5;
+        this.game.sound.setDecodedCallback(this.spell, this.start, this)
     }
     
     update() {
@@ -19,6 +29,7 @@ export class Play extends Phaser.State {
         
         if (this.game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)) {                  
             this.wizard.castSpell();
+            this.spell.play();
         }
     }
 }
